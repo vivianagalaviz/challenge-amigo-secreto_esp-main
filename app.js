1df118b8c6044b4928756c01ca757207efe7b322
Viviana Galaviz <viviana.galaviz3096@alumnos.udg.mx>
2025-09-14 00:19:32 -0600

    Implementa la función para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 //Crear un array para almacenar los nombres
 let amigos = [];
+
+//Implementa una función para agregar amigos
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let amigo = inputAmigo.value.trim();
+    console.log(amigo);
+
+    if (amigo === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(amigo);
+    console.log(amigos);
+    inputAmigo.value = ""; // limpiar el campo después de agregar
+}
