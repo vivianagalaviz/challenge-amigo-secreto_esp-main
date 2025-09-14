f781eb8179479994357b1d489d7de4f5cde3ee6a
Viviana Galaviz <viviana.galaviz3096@alumnos.udg.mx>
2025-09-14 00:04:56 -0600

    Crea un arreglo para los nombres

--- app.js
+++ app.js
@@ -1,16 +1,2 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Crear un array para almacenar los nombres
 let amigos = [];
-
-function agregarAmigo(){
-    let inputAmigo = document.getElementById("amigo");
-    let amigo = inputAmigo.value;
-    //console.log(amigo); 
-
-    if(amigo === ""){
-        alert("Nombre inválido, ingresa otro por favor.");
-        return;
-    }
-
-    amigos.push(amigo);
-    //console.log(amigos);
-}
